bc9c4e199c750135879db42165bcbf21d7093594
Anita RASOA <anita.ras@onja.org>
2021-01-28 09:46:21 +0300

    Finishing the card and the feature components

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,15 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Header, Loading } from '../components';
+import Fuse from 'fuse.js';
+import { Header, Loading, Card } from '../components';
 import * as ROUTES from '../constants/routes';
 import {firebaseContext} from '../context/firebase';
 import SelectProfileContainer from './profiles';
 import FooterContainer from './footer';
 
-export default function BrowseContainer() {
+export default function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({});
     const [category, setCategory] = useState('series');
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
 
     const user = {
         displayName: "Anita",
@@ -21,6 +23,24 @@ export default function BrowseContainer() {
             setLoading(false);
         }, 3000)
     }, [user])
+
+    useEffect(() => {
+        setSlideRows(slides[category]);
+    }, [slides, category])
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, { 
+            keys: ['data.description', 'data.title', 'data.genre'],
+        });
+        const results = fuse.search(searchTerm).map(({ item }) => item);
+            
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results)
+        } else {
+            setSlideRows(slides[category])
+        }
+
+    }, [searchTerm])
 
     return profile.displayName ? (
         <>
@@ -71,6 +91,29 @@ export default function BrowseContainer() {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docsId} item={item}>
+                                    <Card.Image 
+                                        src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} 
+                                    />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}></Card.Feature>
+                    </Card>
+                ))}
+            </Card.Group>
+
             <FooterContainer />
         </> 
     ) : (
